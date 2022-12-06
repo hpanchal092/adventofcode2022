fc45294815089d84221641323ea039cac1ebadb7
Harsh Panchal <hpanchal092@gmail.com>
2022-12-06 09:40:38 -0800

    add day 3

---
+++ day3/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	raw, _ := os.Open("./input.txt")
+
+	data := bufio.NewScanner(raw)
+
+	ans := Part1(data)
+	fmt.Printf("Part 1: %d\n", ans)
+
+	raw.Seek(0, 0)
+	data = bufio.NewScanner(raw)
+
+	ans2 := Part2(data)
+	fmt.Printf("Part 2: %d\n", ans2)
+}
+
+func Part1(data *bufio.Scanner) int {
+	score := 0
+
+	for data.Scan() {
+		rucksack := data.Text()
+		comp1 := rucksack[0 : len(rucksack)/2]
+		comp2 := rucksack[len(rucksack)/2:]
+
+		for i := 0; i < len(comp1); i++ {
+			if strings.ContainsRune(comp1, rune(comp2[i])) {
+				score += priorityOf(comp2[i])
+				break
+			}
+		}
+	}
+
+	return score
+}
+
+func priorityOf(char byte) int {
+	output := 0
+
+	// if capital
+	if char > 64 && char < 91 {
+		output = int(char) - 38
+	}
+
+	// if lowercase
+	if char > 96 && char < 123 {
+		output = int(char) - 96
+	}
+
+	return output
+}
+
+func Part2(data *bufio.Scanner) int {
+	score := 0
+
+	for data.Scan() {
+		rucksack1 := data.Text()
+		data.Scan()
+		rucksack2 := data.Text()
+		data.Scan()
+		rucksack3 := data.Text()
+
+		for i := 0; i < len(rucksack1); i++ {
+            inRucksack2 := strings.ContainsRune(rucksack2, rune(rucksack1[i]))
+            inRucksack3 := strings.ContainsRune(rucksack3, rune(rucksack1[i]))
+            if inRucksack2 && inRucksack3 {
+                score += priorityOf(rucksack1[i])
+                break
+            }
+		}
+	}
+
+	return score
+}
---
+++ day3/main_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"testing"
+)
+
+func TestPart1(t *testing.T) {
+	raw, err := os.Open("./test.txt")
+
+    if err != nil {
+        t.Fatalf("Error opening file, %v", err)
+    }
+
+    want := 157
+
+	data := bufio.NewScanner(raw)
+    ans := Part1(data)
+
+    if ans != want {
+        t.Fatalf("Value returned is %d, want %d", ans, want)
+    }
+}
+
+func TestPriorityOf(t *testing.T) {
+    want := 39
+    ans := priorityOf('M')
+
+    if ans != want {
+        t.Fatalf("Priority returned for m is %d, want %d", want, ans)
+    }
+}
+
+func TestPart2(t *testing.T) {
+	raw, err := os.Open("./test.txt")
+
+    if err != nil {
+        t.Fatalf("Error opening file, %v", err)
+    }
+
+    want := 70
+
+	data := bufio.NewScanner(raw)
+    ans := Part2(data)
+
+    if ans != want {
+        t.Fatalf("Value returned is %d, want %d", ans, want)
+    }
+}
